60299399fa06d40d2f8e6359595f325cd46cc51c
asianParadissseee <asian.paradissseee@gmail.com>
2024-04-15 13:10:26 +0600

    add new options for cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: false,
   });
-  app.use(cors());
+  app.use(
+    cors({
+      origin: ['http://localhost:3000/', 'https://loop-school.vercel.app/'],
+      credentials: true,
+      optionsSuccessStatus: 201,
+    }),
+  );
   const PORT = process.env.PORT || 5050;
   app.enableCors({
     origin: ['http://localhost:3000/', 'https://loop-school.vercel.app/'],
