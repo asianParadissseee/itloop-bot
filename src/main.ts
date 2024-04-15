3d4be0306067395bc5c59447be954195c3b869c3
asianParadissseee <asian.paradissseee@gmail.com>
2024-04-15 12:34:14 +0600

    install types cors and rechange options for enablecors

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,10 @@ async function bootstrap() {
     cors: false,
   });
   const PORT = process.env.PORT || 5050;
-  console.log('это бля bot token ', process.env.BOT_TOKEN);
   app.enableCors({
     origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
   });
   app.use(cors());
   await app.listen(PORT, () =>
