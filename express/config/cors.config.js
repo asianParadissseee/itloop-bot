f25af6fdcb2c4042952a0a408bae3c40588f4d19
asianParadissseee <asian.paradissseee@gmail.com>
2024-04-16 13:30:31 +0600

    add git ignore

--- express/config/cors.config.js
+++ express/config/cors.config.js
@@ -2,8 +2,9 @@ const corsOptions = {
   origin: [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://loop-school.vercel.app/'
+    'https://loop-school.vercel.app'
   ],
+  methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
   credentials: true,
 };
 
